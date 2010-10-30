326ad8bf791e9d82b654b1805fa14530e16dda1e
Kim Blomqvist <kblomqvist@iki.fi>
2010-10-30 15:54:37 +0300

    CLI tool for parsing translation msg from app source files

---
+++ library/Kblom/FunctionParamParser.php
@@ -0,0 +1,269 @@
+<?php
+
+/**
+ * Matches to given function names and parses Parses function  and array keys.
+ *
+ * Common usage: translation resource file generation
+ *
+ * @category  Kblom
+ * @package   Kblom_FunctionParamParser
+ * @copyright Copyright (c) 2010 Kim Blomqvist
+ * @lisence   http://github.com/kblomqvist/Kblom-zf/raw/master/LICENSE The MIT License
+ */
+class Kblom_FunctionParamParser
+{
+    /**
+     * Match to these function names and default pattern if none given
+     * 
+     * Example 1: only default pattern is used
+     *      array('function_name', 'other_function')
+     *
+     * Example 2: mixed default and custom patterns
+     *      array(
+     *          'function_name',
+     *          'other_function' => "^'(.+)',\s*'(.+)'"
+     *      )
+     */
+    protected $_func_kwords = array();
+
+    /** Match to these array keys */
+    protected $_arr_kwords = array();
+
+    /** Paths where to find files. Defaults to path "basepath/." */
+    protected $_basepath;
+    protected $_relativePaths = array('.');
+
+    /** Exclude these paths */
+    protected $_excludePaths = array();
+
+    /** Reads only files with these extensions */
+    protected $_extensions = array('php', 'phtml');
+
+    /** Parse nested directories of given paths */
+    protected $_parseRecursively = true;
+
+    /** Get the string inside matched function parenthesis; func(THIS_PORTION) */
+    protected $_defaultPattern = '(.+)';
+
+    protected $_matches;
+
+    public function __construct(array $options = array())
+    {
+        $this->setOptions($options);
+    }
+
+    public function setOptions(array $options)
+    {
+        if (isset($options['funcKeyWords'])) {
+            $this->setFuncKeyWords($options['funcKeyWords']);
+        }
+        if (isset($optios['arrKeyWords'])) {
+            $this->setArrKeyWords($options['arrKeyWords']);
+        }
+        if (isset($options['basepath'])) {
+            $this->setBasepath($options['basepath']);
+        }
+        if (isset($options['relativePaths'])) {
+            $this->setRelativePaths($options['relativePaths']);
+        }
+        if (isset($options['excludePaths'])) {
+            $this->setExcludePaths($options['excludePaths']);
+        }
+        if (isset($options['parseRecursively'])) {
+            $this->setParseRecursively($options['parseRecursively']);
+        }
+
+        return $this;
+    }
+
+    public function setFuncKeyWords(array $func_kwords)
+    {
+        $this->_func_kwords = $func_kwords;
+        return $this;
+    }
+
+    public function setArrKeyWords(array $arr_kwords)
+    {
+        $this->_arr_kwords = $arr_kwords;
+        return $this;
+    }
+
+    public function setBasepath($path)
+    {
+        $this->_basepath = (string) $path;
+        return $this;
+    }
+
+    public function setRelativePaths(array $paths) {
+        $this->_relativePaths = array();
+        foreach ($paths as $path) {
+            $this->_relativePaths[] = trim($path, '/');
+        }
+        return $this;
+    }
+
+    public function setExcludePaths(array $paths) {
+        $this->_excludePaths = array();
+        foreach ($paths as $path) {
+            $this->_excludePaths[] = trim($path, '/');
+        }
+        return $this;
+    }
+
+    public function setDefaultPattern($pattern)
+    {
+        $this->_defaultPattern = (string) $pattern;
+        return $this;
+    }
+
+    public function setParseRecursively($state)
+    {
+        $this->_parseRecursively = (boolean) $state;
+        return $this;
+    }
+
+    public function getMatches($include_keys = true, $reparse = false)
+    {
+        if (!isset($this->_matches) || (true === $reparse)) {
+            $this->_matches = array();
+
+            foreach ($this->_relativePaths as $path) {
+                $path = $this->_basepath . '/' . $path;
+                $this->_matches = $this->_catMatches(
+                    $this->_matches, $this->parseFolder($path, $this->_parseRecursively)
+                );
+            }
+        }
+
+        if (false === $include_keys) {
+            $ret = array();
+            foreach ($this->_matches as $func => $matches) {
+                $ret = array_merge($ret, $matches);
+            }
+            return $ret;
+        }
+
+        return $this->_matches;
+    }
+
+    public function parseFile($file, $throw = false)
+    {
+        $path_parts = pathinfo($file);
+        if (!in_array($path_parts['extension'], $this->_extensions)) {
+            return array();
+        }
+
+        if (!is_file($file) || !is_readable($file)) {
+            if (true === $throw) {
+                throw new Zend_Exception($file . ' is not a file or is not readable.');
+            }
+            return array();
+        }
+
+        // Read file
+        $f_content = file_get_contents($file);
+
+        $matches = array();
+        foreach ($this->_func_kwords as $func => $patterns) {
+            if (is_int($func)) {
+                $func = $patterns;
+                $patterns = array($this->_defaultPattern);
+            }
+
+            // Look up for functions and catch params part
+            preg_match_all("/{$func}\(\s*(.+)\s*\)/", $f_content, $t_array);
+            $func_params = $t_array[1];
+            unset($t_array);
+
+            // Match set patterns against function's params part
+            $matches[$func] = array();
+            $pattern = implode($patterns, '|');
+            foreach ($func_params as $params) {
+                $matches[$func] = array_merge(
+                    $matches[$func], $this->_parseFuncParams("/{$pattern}/", $params)
+                );
+            }
+        }
+
+        // Look up for array keys and catch value
+        $matches['__arr_keys'] = array();
+        foreach ($this->_arr_kwords as $key) {
+            preg_match_all("/'{$key}'\s*=>\s*'(.+)'/", $f_content, $t_array);
+            $matches['__arr_keys'] = array_merge($matches['__arr_keys'], $t_array[1]);
+            unset($t_array);
+        }
+
+        unset($f_content);
+
+        return $matches;
+    }
+
+    public function parseFolder($folder, $recursive = true)
+    {
+        if (substr($folder, -1) === '/') {
+            $folder = substr_replace($folder, '', -1);
+        }
+        if ($this->isExcludedPath($folder)) {
+            return array();
+        }
+
+        $files = scandir($folder);
+        unset($files[0], $files[1]); // remove files . and ..
+
+        $matches = array();
+        foreach($files as $file) {
+            if (is_dir($folder . '/' . $file) && (true === $recursive)) {
+                // Recursion, traverses through subdirs
+                $matches = $this->_catMatches(
+                    $matches, $this->parseFolder($folder . '/' . $file)
+                );
+            } else {
+                $matches = $this->_catMatches(
+                    $matches, $this->parseFile($folder . '/' . $file)
+                );
+            }
+        }
+
+        return $matches;
+    }
+
+    public function isExcludedPath($path)
+    {
+        $path = (string) $path;
+        foreach ($this->_excludePaths as $p) {
+            if ($this->_basepath . '/' . $p == $path) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function _catMatches($a1, $a2)
+    {
+        foreach ($a2 as $func => $matches) {
+            if (!isset($a1[$func])) {
+                $a1[$func] = array();
+            }
+            $a1[$func] = array_merge($a1[$func], $matches);
+        }
+
+        return $a1;
+    }
+
+    private function _parseFuncParams($pattern, $params)
+    {
+        preg_match($pattern, $params, $matches);
+        unset($matches[0]);
+
+        $ret = array();
+        foreach ($matches as $match) {
+            if (!empty($match)) {
+                $ret[] = $match;
+            }
+        }
+        unset($matches);
+
+        return $ret;
+    }
+}
+
---
+++ library/Kblom/Tool/Locale.php
@@ -0,0 +1,205 @@
+<?php
+
+require_once 'Kblom/FunctionParamParser.php';
+
+/**
+ * CLI tool for translation resource file generation
+ *
+ * @category  Kblom
+ * @package   Kblom_Tool_Locale
+ * @copyright Copyright (c) 2010 Kim Blomqvist
+ * @lisence   http://github.com/kblomqvist/Kblom-zf/raw/master/LICENSE The MIT License
+ */
+class Kblom_Tool_Locale extends Zend_Tool_Project_Provider_Abstract
+{
+    /** Default patter for function parameter parsing */
+    const DEFAULT_PATTERN = "^'(.+)'\s*,|^'(.+)'$";
+
+    /**
+     * Parser matches to these function names and parses
+     * function parameters with the given regexp pattern.
+     */
+    protected $_func_kwords = array(
+        'translate' => array(
+            self::DEFAULT_PATTERN,
+            "^array\(\s*'(.+)'\s*,\s*'(.+)'",
+        ),
+        'plural' => array(
+            "^'(.+)',\s*'(.+)'",
+        ),
+        'setLabel',
+        'setDescription'
+    );
+
+    /** Parser matches to these array key names */
+    protected $_arr_kwords = array(
+        'label',
+        'legend',
+        'description',
+    );
+
+    /** Supported adapters */
+    protected $_adapters = array(
+        'array'
+    );
+
+    public function create($locale, $module = 'all',
+                           $adapter = 'array', $kwords = null)
+    {
+        $response = $this->_registry->getResponse();
+        $profile  = $this->_loadProfile(self::NO_PROFILE_THROW_EXCEPTION);
+
+        /** Check adapter support */
+        if (!in_array($adapter, $this->_adapters)) {
+            return $this->_responseUnknownAdapter($adapter, $response);
+        }
+
+        /** Resolve paths */
+        $modulePath = $this->_getModulePath($module, $profile);
+        if (!file_exists($modulePath)) {
+            return $this->_responseModuleDoesNotExist($module, $response);
+        }
+        $localesPath = $this->_getLocalesPath($locale, $module, $profile);
+
+        /** Add additional key words */
+        if (null !== $kwords) {
+            $this->_addKeyWords($kwords);
+        }
+
+        /** Setup function param parser */
+        $parser = new Kblom_FunctionParamParser(array(
+            'basepath' => $modulePath,
+            'funcKeyWords' => $this->_func_kwords,
+            'arrKeyWords' => $this->_arr_kwords,
+            'defaultPattern' => self::DEFAULT_PATTERN
+        ));
+        if ($module == 'default' || $module == 'application') {
+            $parser->setExcludePaths(array('modules'));
+        }
+
+        /**
+         * @TODO refactor beginning here...
+         * - Save methods for different adapters
+         * - It may be better use Zend_Translate to read current translations
+         */
+        $translations = $this->_loadTranslations($localesPath . "/$module.php");   
+        $matches = $parser->getMatches(false);
+
+        $i = 0; $addedMessages = '';
+        foreach ($matches as $id) {
+            if (!isset($translations[$id])) {
+                $i++;
+                $translations[$id] = '';
+                $addedMessages .= "$id\n";
+            }
+        }
+
+        if ($i) {
+            $response->appendContent("Added $i message id(s)", array('color' => 'green'));
+            $response->appendContent($addedMessages);
+        } else {
+            $response->appendContent("No new message id(s)", array('color' => 'green'));
+        }
+        
+        $i = 0; $removedMessages = '';
+        foreach ($translations as $id => $msg) {
+            if (!in_array($id, $matches)) {
+                $i++;
+                unset($translations[$id]);
+                $removedMessages .= "$id\n";
+            }
+        }
+
+        if ($i) {
+            $response->appendContent("Removed $i message id(s)", array('color' => 'yellow'));
+            $response->appendContent($removedMessages);
+        } else {
+            $response->appendContent("No removed message id(s)", array('color' => 'green'));
+        }
+
+        $this->_saveTranslations($localesPath, "$module.php", $translations);
+
+        return true;
+    }
+
+    protected function _getModulePath($module, $profile)
+    {
+        $path = $profile->search('ApplicationDirectory')->getPath();
+
+        if ($module == 'all' || $module == 'application') {
+            return $path;
+        }
+
+        return $path . "/modules/$module";
+    }
+
+    protected function _getLocalesPath($locale, $module, $profile)
+    {
+        $path = $profile->search('LocalesDirectory')->create()->getPath();
+        $path .= "/$locale";
+
+        if ($module == 'all' || $module == 'application') {
+            return $path;
+        }
+
+        return $path . "/modules";
+    }
+
+    protected function _addKeyWords($list)
+    {
+        $kwords = explode(':', (string) $list);
+        foreach ($kwords as $k) {
+            preg_match('/^\[(.+)\]$/', $k, $arr_key);
+            if (isset($arr_key[1])) {
+                $this->_arr_kwords[] = $arr_key[1];
+            } else {
+                $this->_func_kwords[] = $k;
+            }
+        }
+    }
+
+    protected function _getParser($path)
+    {
+        $parser = new Muiku_Tool_FunctionParamParser($path);
+        $parser->setFuncKeyWords($this->_func_kwords);
+        $parser->setArrKeyWords($this->_arr_kwords);
+        $parser->setDefaultPattern(self::DEFAULT_PATTERN);
+        $parser->setRelativePaths(array('.'));
+
+        return $parser->getMatches();
+    }
+
+    protected function _loadTranslations($filename)
+    {
+        $data = array();
+        if (file_exists($filename)) {
+            ob_start();
+            $data = include($filename);
+            ob_end_clean();
+        }
+        return $data;
+    }
+
+    protected function _saveTranslations($path, $filename, array $translations)
+    {
+        if (!file_exists($path)) {
+            mkdir($path);
+        }
+
+        return file_put_contents($path . "/$filename", "<?php\nreturn " . var_export($translations, true) . ';');
+    }
+
+    protected function _responseUnknownAdapter($adapter, $response)
+    {
+        $response->appendContent("$adapter: unknown adapter", array('color' => 'yellow'));
+
+        return false;
+    }
+
+    protected function _responseModuleDoesNotExist($module, $response)
+    {
+        $response->appendContent("$module: module does not exist", array('color' => 'yellow'));
+
+        return false;
+    }
+}
